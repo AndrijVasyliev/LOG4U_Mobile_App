5d049ee0c1d9b796437e758914d22c3ee5678c40
Kozak_Mamaj <aa5856bk@gmail.com>
2023-10-18 18:27:16 +0300

    feat(LOG): home screen (WIP)

---
+++ constants/icons.js
@@ -0,0 +1,29 @@
+import heart from "../assets/icons/heart.png";
+import menu from "../assets/icons/menu.png";
+import search from "../assets/icons/search.png";
+import filter from "../assets/icons/filter.png";
+import left from "../assets/icons/left.png";
+import heartOutline from "../assets/icons/heart-ol.png";
+import share from "../assets/icons/share.png";
+import location from "../assets/icons/location.png";
+import chevronLeft from '../assets/icons/chevron-left.png';
+import chevronRight from '../assets/icons/chevron-right.png';
+import LOGO_4U_Red from '../assets/icons/4U_Red.png';
+import LOGO_4U_White from '../assets/icons/4U_White.png';
+import AccountCircle from '../assets/icons/account_circle_white.png';
+
+export default {
+  heart,
+  menu,
+  search,
+  filter,
+  left,
+  heartOutline,
+  share,
+  location,
+  chevronLeft,
+  chevronRight,
+  LOGO_4U_Red,
+  LOGO_4U_White,
+  AccountCircle,
+};
