935224dbc730db6bf59d3b85dba4163f02893796
Kozak_Mamaj <aa5856bk@gmail.com>
2023-11-15 15:19:34 +0200

    feat(LOG): set app name

--- constants/general.ts
+++ constants/general.ts
@@ -7,9 +7,8 @@ export const SET_LOCATION_PATH = '/mobileApp/setTruckLocation';
 export const LOCATION_TRACKING = 'location-tracking';
 export const BACKGROUND_FETCH_TASK = 'background-fetch';
 export const BACKGROUND_GEOFENCE_TASK = 'background-geofence';
-export const LOCATION_NOTIFICATION_BODY =
-  '4U LOGISTICS is tracking your location';
-export const LOCATION_NOTIFICATION_TITLE = '4U LOGISTICS Location Service';
+export const LOCATION_NOTIFICATION_BODY = '4U TRACK is tracking your location';
+export const LOCATION_NOTIFICATION_TITLE = '4U TRACK Location Service';
 export const LOCATION_UPDATE_INTERVAL = 1000 * 60 * 30;
 export const LOCATION_DISTANCE_INTERVAL = 250;
 export const GEOFENCE_DISTANCE_INTERVAL = 300;
