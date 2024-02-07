c7d6498154b06475269f867c8e08b1ce6642ded5
Kozak_Mamaj <aa5856bk@gmail.com>
2024-02-07 10:48:58 +0200

    feat(LOG): not found back button

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,37 +1,56 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Link, Stack } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 
 import { COLORS } from '../constants';
 
 const NotFoundScreen = () => {
+  const router = useRouter();
+  const handleBack = () => {
+    if (router.canGoBack()) {
+      router.back();
+    } else {
+      router.replace('/home');
+    }
+  };
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesnt exist.</Text>
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
-        </Link>
+        <Text style={styles.title}>Requested content does not exist.</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleBack}>
+            <Text style={styles.buttonText}>Back</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: COLORS.primary,
+    borderRadius: 10,
+    height: 40,
+    justifyContent: 'center',
+    width: '100%',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    flex: 1,
+    height: 40,
+    justifyContent: 'center',
+    marginTop: 5,
+    width: '100%',
+  },
+  buttonText: { color: COLORS.white },
   container: {
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
     padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    color: COLORS.gray2,
-    fontSize: 14,
   },
   title: {
     fontSize: 20,
